b97b0333e12a2ae505b22b57acf787e9f6b17ab7
Erdem Baki Günaydın <erdembakigunaydn28@gmail.com>
2024-03-21 06:01:59 +0300

    “First”

--- workingPython.py
+++ workingPython.py
@@ -1,15 +1,15 @@
-1--"""boycm = int(input("Lütfen boyunuzu (santimetre cinsinde) girin: "))
+1--boycm = int(input("Lütfen boyunuzu (santimetre cinsinde) girin: "))
 ağirlik = int(input("Lütfen ağırlığınızı (kilogram cinsinde) girin: "))
 boym = boycm / 100
 vki = ağirlik / (boym * boym)
-print("Vücut Kitle İndeksiniz (VKİ):", vki)"""
+print("Vücut Kitle İndeksiniz (VKİ):", vki)
 
-2--"""maas = int (input('lütfen maasinizi giriniz'))
+2-- maas = int (input('lütfen maasinizi giriniz'))
 zamOrani = int (input(lütfen zam oraninizi giriniz))
 employee= maas+(maas*(zamOrani/100))
-print ('yeni maaşınız=',employee)"""
+print ('yeni maaşınız=',employee)
 
-3--"""sayi1 = int(input("Birinci sayıyı giriniz: "))
+3-- sayi1 = int(input("Birinci sayıyı giriniz: "))
 sayi2 = int(input("İkinci sayıyı giriniz: "))
 sayi3 = int(input("Üçüncü sayıyı giriniz: "))
 
@@ -21,15 +21,15 @@ if sayi2 > sayi1:
 if sayi3 > sayi2:
     enbüyük = sayi3
 
-print("En büyük sayı:", enbüyük)"""
+print("En büyük sayı:", enbüyük)
 
-4--"""import math
+4-- import math
 
 yaricap = int(input("Yari Capi giriniz: "))
 cevre = 2 * math.pi * yaricap
 alan = math.pi * yaricap**2
 print ("Dairenin çevresi", cevre)
-print ("Dairenin alanı", alan)"""
+print ("Dairenin alanı", alan)
 
 
 
